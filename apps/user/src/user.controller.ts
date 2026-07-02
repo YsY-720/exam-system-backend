d85c86f7acbe37c4fd86621790af3a71007d4357
yusiyuan <1378209949@qq.com>
2026-07-02 18:21:38 +0800

    feat：添加redis、mysql模块

--- apps/user/src/user.controller.ts
+++ apps/user/src/user.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
-import { UserService } from './user.service';
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { UserService } from "./user.service";
+import { RegisterUserDto } from "./dto/register-user.dto";
 
 @Controller()
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) {
+  }
 
   @Get()
   getHello(): string {
     return this.userService.getHello();
+  }
+
+  @Post("register")
+  async register(@Body() registerUser: RegisterUserDto) {
+    return await this.userService.create(registerUser);
   }
 }
--- apps/user/src/user.service.ts
+++ apps/user/src/user.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { RegisterUserDto } from "./dto/register-user.dto";
 
 @Injectable()
 export class UserService {
   getHello(): string {
-    return 'Hello World!';
+    return "Hello World!";
+  }
+
+  async create(data: RegisterUserDto) {
+    return {
+      username: data.username,
+      email: data.email
+    };
   }
 }
